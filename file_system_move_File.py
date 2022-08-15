9b8a1f9ca11ce1a0e596d08a39afb441772ceb80
sanillandge <82667082+sanillandge@users.noreply.github.com>
2022-08-15 19:04:43 +0530

    Add files via upload

---
+++ file_system_move_File.py
@@ -0,0 +1,9 @@
+import os
+
+src="C:/Users/hp/OneDrive/Desktop/Python/File.txt"
+
+dest = "C:/Users/hp/OneDrive/Desktop/Python/New File.txt"
+
+os.rename(src,dest)
+
+print("Source path has been renamed successfully!")
